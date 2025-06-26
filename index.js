7f1a9494e5ba99347da2db37b3cf435268909edb
eric2025.12 <ericmuigaikims.gmail.com>
2025-06-26 12:14:24 +0300

    dom modification

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// Remove the <main> element from the DOM
+const main = document.getElementById('main');
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new element
+newHeader.id = 'victory';
+
+// Set the text inside the new <h1> element
+newHeader.textContent = 'Eric is the champion';
+
+// Append the new element to the body of the document
+document.body.append(newHeader);
